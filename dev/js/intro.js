a99e1c02c2109bf9b810c90ba1a1fd9d71334bae
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-11-18 15:18:57 -0500

    blah

--- dev/js/intro.js
+++ dev/js/intro.js
@@ -1,18 +1,72 @@
 import {gsap} from "gsap";
-import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+// import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+// import {MorphSVGPlugin} from "gsap/DrawSVGPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin);
+// gsap.registerPlugin(MorphSVGPlugin);
 
-// var RBSpeed = .8;
+var RBSpeed = .8;
 
-// var tl = gsap.timeline({ease: "power2.out"});
+var tl = gsap.timeline({ease: "power2.out"});
 
 
-const introTL = gsap.timeline();
+// const tl = gsap.timeline();
 
 
 export function IntroAnimation() {
-    introTL.from("#spikey-shape-big", {duration: 1.5, scale: 0, transformOrigin: "center"}, "play")
+        
+        tl.from(".Logo", {
+            duration: RBSpeed,
+            x: 250
+        }, "play");
+        
+        tl.to("#B-1", {
+            duration: .4,
+            scaleX:1.1
+        }, "play");
+        
+        tl.to("#B-2", {
+            duration: .4,
+            scaleX:1.2
+        }, "play");
+        
+        tl.to("#B-1", {
+            duration: .4,
+            scaleX:1
+        }, "play2");
+        
+        tl.to("#B-2", {
+            duration: .4,
+            scaleX:1
+        }, "play2");
+        
+        tl.to("#back-Bs", {
+            transformOrigin: "center", 
+            duration: 1.25,
+            scaleX:0,
+            scaleY:0
+        }, "play3");
 
-    return introTL;
+        // tl.to("#R",{duration:1, morphSVG:"#door"},"play3")
+
+        tl.to("#Republic-Bank", {
+            duration: 1.5,
+            opacity:1
+        }, "play4");
+        
+        tl.to("#tagline", {
+            duration: 1.5,
+            opacity:1
+        }, "play4");
+        
+        tl.from("#tagline", {
+            duration: 1.5,
+            x: 100
+        }, "play4");
+        
+        tl.to("#R", {
+            duration: 1.5,
+            fill: "rgb(239, 85, 86)"
+        }, "play4");
+
+    return tl;
 }
